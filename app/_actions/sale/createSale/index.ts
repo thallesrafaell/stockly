a379c6a7f652647a506099ce8c8e65a6178d2e62
Thalles <thallesrafaell@outlook.com>
2025-06-08 23:08:38 -0300

    refactor: :recycle: optimize createSaleAction by using transaction for product operations

--- app/_actions/sale/createSale/index.ts
+++ app/_actions/sale/createSale/index.ts
@@ -6,52 +6,55 @@ import { CreateSaleSchema, createSaleSechema } from "./schema";
 
 export const createSaleAction = async (data: CreateSaleSchema) => {
   createSaleSechema.parse(data);
-
-  const sale = await db.sale.create({
-    data: {
-      date: new Date(),
-    },
-  });
-
-  for (const product of data.products) {
-    const foundProduct = await db.product.findUnique({
-      where: { id: product.id },
-    });
-    if (foundProduct === undefined) {
-      throw new Error(`Produto com ID ${product.id} não encontrado`);
-    }
-    const productStock = foundProduct?.stock;
-
-    if (productStock === undefined) {
-      throw new Error(`Estoque do produto com ID ${product.id} não encontrado`);
-    }
-    const stock = product.quantity > productStock;
-
-    if (stock) {
-      throw new Error(
-        `Quantidade solicitada para o produto com ID ${product.id} excede o estoque disponível`,
-      );
-    }
-
-    const unitPrice = foundProduct?.price;
-
-    await db.saleProduct.create({
+  await db.$transaction(async (tx) => {
+    const sale = await tx.sale.create({
       data: {
-        productId: product.id,
-        saleId: sale.id,
-        quantity: product.quantity,
-        unitPrice: Number(unitPrice),
+        date: new Date(),
       },
     });
 
-    await db.product.update({
-      where: { id: product.id },
-      data: {
-        stock: {
-          decrement: product.quantity,
+    for (const product of data.products) {
+      const foundProduct = await db.product.findUnique({
+        where: { id: product.id },
+      });
+      if (foundProduct === undefined) {
+        throw new Error(`Produto com ID ${product.id} não encontrado`);
+      }
+      const productStock = foundProduct?.stock;
+
+      if (productStock === undefined) {
+        throw new Error(
+          `Estoque do produto com ID ${product.id} não encontrado`,
+        );
+      }
+      const stock = product.quantity > productStock;
+
+      if (stock) {
+        throw new Error(
+          `Quantidade solicitada para o produto com ID ${product.id} excede o estoque disponível`,
+        );
+      }
+
+      const unitPrice = foundProduct?.price;
+
+      await tx.saleProduct.create({
+        data: {
+          productId: product.id,
+          saleId: sale.id,
+          quantity: product.quantity,
+          unitPrice: Number(unitPrice),
         },
-      },
-    });
-  }
-  revalidateTag("get-products");
+      });
+
+      await tx.product.update({
+        where: { id: product.id },
+        data: {
+          stock: {
+            decrement: product.quantity,
+          },
+        },
+      });
+    }
+    revalidateTag("get-products");
+  });
 };
