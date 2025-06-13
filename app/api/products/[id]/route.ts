d74ded8c91fa9c93a167d25ab9d0c6f7d3de51ac
Thalles <thallesrafaell@outlook.com>
2025-06-13 19:19:20 -0300

    refactor: :recycle: update GET and DELETE functions to use context for parameters

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,7 +1,9 @@
-
 import { db } from "@/app/_lib/prisma";
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-  const { id } = params;
+export async function GET(
+  request: Request,
+  context: { params: { id: string } }, // Alterado aqui
+) {
+  const { id } = context.params; // E aqui
 
   const product = await db.product.findUnique({
     where: { id },
@@ -19,26 +21,35 @@ export async function GET(request: Request, { params }: { params: { id: string }
   });
 }
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
   const { id } = params;
 
   try {
     await db.product.delete({
       where: { id },
     });
-    
+
     // Com status 204, retornamos apenas a Response sem corpo
     return new Response(null, { status: 204 });
-  } catch (error: any) {
-    if (error.code === 'P2025') {
+  } catch (error) {
+    // Verifica se o erro é uma instância de Error para acessar 'message' e 'code'
+    if (error instanceof Error && "code" in error && error.code === "P2025") {
       return Response.json(
         { error: "Produto não encontrado" },
-        { status: 404 }
+        { status: 404 },
       );
     }
+    // Se for um erro conhecido com mensagem, usa a mensagem do erro
+    if (error instanceof Error) {
+      return Response.json({ error: error.message }, { status: 500 });
+    }
+    // Fallback para erros desconhecidos
     return Response.json(
-      { error: error.message },
-      { status: 500 }
+      { error: "Ocorreu um erro inesperado" },
+      { status: 500 },
     );
   }
 }
